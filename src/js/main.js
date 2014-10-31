1a0b6f93652c8dedf9250f04f68a97e2a12e0330
Étienne Vallette d'Osia <evo@zengularity.com>
2014-10-31 23:25:29 +0900

    Add awesome slider

--- src/js/main.js
+++ src/js/main.js
@@ -45,20 +45,38 @@ if (navigator.getUserMedia === undefined) {
 var Buffer = function (tpl) {
   var size = tpl.size;
   var elems = new Array(size);
-  var idx = 1;
+  var idx = 0;
   function put(el) {
     elems[idx] = el;
     idx = (idx + 1) % size;
     return elems[idx];
   }
+  function updateSize(newSize) {
+    var newElems = new Array(newSize);
+    var m = Math.min(size, newSize);
+    for (var i = 0; i < m; i++) {
+      newElems[i] = elems[(idx + i) % size];
+    }
+    elems = newElems;
+    size = newSize;
+    idx = 0;
+  }
   return {
     size,
-    put
+    put,
+    updateSize,
   };
 };
 
 var videoLeft  = document.querySelector('#left-video');
 var videoRight = document.querySelector('#right-video');
+
+var lagSlider = document.getElementById("lag-slider");
+var lagValue = lagSlider.value;
+var lagShown = document.getElementById("lag-shown");
+lagShown.innerHTML = lagValue;
+
+var refreshValue = 33;
 
 navigator.getUserMedia({video: true, audio: true}, localMediaStream => {
   videoLeft.src = window.URL.createObjectURL(localMediaStream);
@@ -121,8 +139,23 @@ function initVideo() {
     }
   }, false);
 
-  var leftBuffer = Buffer({size: 30});
-  var rightBuffer = Buffer({size: 30});
+  function computeBufferSize() {
+    if (lagValue === 0) {
+      return 1;
+    } else {
+      return parseInt(lagValue/refreshValue * 1000);
+    }
+  }
+  var leftBuffer = Buffer({size: computeBufferSize()});
+  var rightBuffer = Buffer({size: computeBufferSize()});
+
+  function setLag() {
+    lagValue = lagSlider.value;
+    lagShown.innerHTML = lagValue;
+    leftBuffer.updateSize(computeBufferSize());
+    rightBuffer.updateSize(computeBufferSize());
+  }
+  lagSlider.addEventListener("change", setLag);
 
   // Every 33 milliseconds copy the video image to the canvas
   videoLeft.addEventListener('play', function() {
