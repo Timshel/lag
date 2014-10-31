e84ed1a4ee7da0d21f0e4a7a5ab6fe323fc15138
Étienne Vallette d'Osia <evo@zengularity.com>
2014-10-31 22:00:19 +0900

    wip

--- src/js/main.js
+++ src/js/main.js
@@ -34,28 +34,68 @@ document.getElementById("fullscreen").addEventListener("click", function(){
   launchIntoFullscreen(dive);
 }, false);
 
+if (navigator.getUserMedia === undefined) {
+  navigator.getUserMedia = (
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia
+  );
+}
+
 function initVideo() {
-  if (navigator.getUserMedia === undefined) {
-    navigator.getUserMedia = (
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia ||
-      navigator.msGetUserMedia
-    );
-  }
+
+  var videoLeft = document.querySelector('#left');
+  // var videoRight = document.querySelector('#right');
+
+  var leftCanvasHidden = document.getElementById('left-hidden');
+  var leftCanvasShown = document.getElementById('left-shown');
+  var leftCanvasHiddenCon = leftCanvasHidden.getContext('2d');
+  var leftCanvasShownCon = leftCanvasShown.getContext('2d');
+  var w = 600;
+  var h = 420;
+
+  var initialized = false;
+  videoLeft.addEventListener('canplay', e => {
+    if (!initialized) {
+      // videoWidth isn't always set correctly in all browsers
+      if (videoLeft.videoWidth > 0) {
+        h = videoLeft.videoHeight / (videoLeft.videoWidth / w);
+      }
+      leftCanvasHidden.setAttribute('width', w);
+      leftCanvasHidden.setAttribute('height', h);
+      leftCanvasShown.setAttribute('width', w);
+      leftCanvasShown.setAttribute('height', h);
+      // Reverse the canvas image
+      leftCanvasHiddenCon.translate(w, 0);
+      leftCanvasHiddenCon.scale(-1, 1);
+      leftCanvasShownCon.translate(w, 0);
+      leftCanvasShownCon.scale(-1, 1);
+      initialized = true;
+    }
+  }, false);
+  // Every 33 milliseconds copy the video image to the canvas
+  videoLeft.addEventListener('play', function() {
+    setInterval(function() {
+      if (videoLeft.paused || videoLeft.ended) return;
+      leftCanvasHiddenCon.fillRect(0, 0, w, h);
+      leftCanvasHiddenCon.drawImage(videoLeft, 0, 0, w, h);
+      leftCanvasShownCon.fillRect(0, 0, w, h);
+      // leftCanvasShownCon.drawImage(videoLeft, 0, 0, w, h);
+      leftCanvasShownCon.putImageData(leftCanvasHiddenCon.getImageData(), 0, 0, w, h);
+    }, 33);
+  }, false);
+
   if (navigator.getUserMedia === undefined) {
     console.error("getUserMedia() not found");
     return;
   }
   navigator.getUserMedia({video: true, audio: true}, localMediaStream => {
-    var videoLeft = document.querySelector('#left');
     videoLeft.src = window.URL.createObjectURL(localMediaStream);
     videoLeft.onloadedmetadata = function(e) {};
-
-    navigator.getUserMedia({video: true}, localMediaStream => {
-      var videoRight = document.querySelector('#right');
-      videoRight.src = window.URL.createObjectURL(localMediaStream);
-      videoRight.onloadedmetadata = function(e) {};
-    }, e => console.error('Error during video acquisition:', e));
+    // navigator.getUserMedia({video: true}, localMediaStream => {
+    //   videoRight.src = window.URL.createObjectURL(localMediaStream);
+    //   videoRight.onloadedmetadata = function(e) {};
+    // }, e => console.error('Error during video acquisition:', e));
   }, e => console.error('Error during video acquisition:', e));
 }
 initVideo();
