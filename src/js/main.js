f0b374d518c602362f245265aaefe91814a21226
Étienne Vallette d'Osia <evo@zengularity.com>
2014-10-31 22:00:19 +0900

    Use canvas

--- src/js/main.js
+++ src/js/main.js
@@ -34,28 +34,90 @@ document.getElementById("fullscreen").addEventListener("click", function(){
   launchIntoFullscreen(dive);
 }, false);
 
+if (navigator.getUserMedia === undefined) {
+  navigator.getUserMedia = (
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia
+  );
+}
+
 function initVideo() {
-  if (navigator.getUserMedia === undefined) {
-    navigator.getUserMedia = (
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia ||
-      navigator.msGetUserMedia
-    );
+
+  var videoLeft = document.querySelector('#left-video');
+  var videoRight = document.querySelector('#right-video');
+
+  function getCanvasAndContext(id) {
+    var canvas = document.getElementById(id);
+    var conn = canvas.getContext('2d');
+    return [canvas, conn];
   }
+
+  var [leftCanvasHidden, leftCanvasHiddenCon] = getCanvasAndContext('left-hidden');
+  var [leftCanvasShown, leftCanvasShownCon] = getCanvasAndContext('left-shown');
+  var [rightCanvasHidden, rightCanvasHiddenCon] = getCanvasAndContext('right-hidden');
+  var [rightCanvasShown, rightCanvasShownCon] = getCanvasAndContext('right-shown');
+  var w = 600;
+  var h = 420;
+
+  var initialized = false;
+  videoLeft.addEventListener('canplay', e => {
+    if (!initialized) {
+      // videoWidth isn't always set correctly in all browsers
+      if (videoLeft.videoWidth > 0) {
+        h = videoLeft.videoHeight / (videoLeft.videoWidth / w);
+      }
+      leftCanvasHidden.setAttribute('width', w);
+      leftCanvasHidden.setAttribute('height', h);
+      leftCanvasShown.setAttribute('width', w);
+      leftCanvasShown.setAttribute('height', h);
+      rightCanvasHidden.setAttribute('width', w);
+      rightCanvasHidden.setAttribute('height', h);
+      rightCanvasShown.setAttribute('width', w);
+      rightCanvasShown.setAttribute('height', h);
+      // Reverse the canvas image
+      leftCanvasHiddenCon.translate(w, 0);
+      leftCanvasHiddenCon.scale(-1, 1);
+      leftCanvasShownCon.translate(w, 0);
+      leftCanvasShownCon.scale(-1, 1);
+      rightCanvasHiddenCon.translate(w, 0);
+      rightCanvasHiddenCon.scale(-1, 1);
+      rightCanvasShownCon.translate(w, 0);
+      rightCanvasShownCon.scale(-1, 1);
+      initialized = true;
+    }
+  }, false);
+  // Every 33 milliseconds copy the video image to the canvas
+  videoLeft.addEventListener('play', function() {
+    setInterval(function() {
+      if (videoLeft.paused || videoLeft.ended) return;
+      leftCanvasHiddenCon.fillRect(0, 0, w, h);
+      leftCanvasHiddenCon.drawImage(videoLeft, 0, 0, w, h);
+      leftCanvasShownCon.fillRect(0, 0, w, h);
+      leftCanvasShownCon.putImageData(leftCanvasHiddenCon.getImageData(0, 0, w, h), 0, 0);
+    }, 33);
+  }, false);
+  videoRight.addEventListener('play', function() {
+    setInterval(function() {
+      if (videoRight.paused || videoRight.ended) return;
+      rightCanvasHiddenCon.fillRect(0, 0, w, h);
+      rightCanvasHiddenCon.drawImage(videoRight, 0, 0, w, h);
+      rightCanvasShownCon.fillRect(0, 0, w, h);
+      rightCanvasShownCon.putImageData(rightCanvasHiddenCon.getImageData(0, 0, w, h), 0, 0);
+    }, 33);
+  }, false);
+
   if (navigator.getUserMedia === undefined) {
     console.error("getUserMedia() not found");
     return;
   }
   navigator.getUserMedia({video: true, audio: true}, localMediaStream => {
-    var videoLeft = document.querySelector('#left');
     videoLeft.src = window.URL.createObjectURL(localMediaStream);
     videoLeft.onloadedmetadata = function(e) {};
-
-    navigator.getUserMedia({video: true}, localMediaStream => {
-      var videoRight = document.querySelector('#right');
+    // navigator.getUserMedia({video: true}, localMediaStream => {
       videoRight.src = window.URL.createObjectURL(localMediaStream);
-      videoRight.onloadedmetadata = function(e) {};
-    }, e => console.error('Error during video acquisition:', e));
+      // videoRight.onloadedmetadata = function(e) {};
+    // }, e => console.error('Error during video acquisition:', e));
   }, e => console.error('Error during video acquisition:', e));
 }
 initVideo();
