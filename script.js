cd7007e7fb8a54e3551ce310d63cebeeb6cb20df
Jacques <jba@zengularity.com>
2014-10-31 10:36:22 +0100

    Yeah fullscreen :)

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+// Find the right method, call on correct element
+function launchIntoFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
+function isFullScreen(){
+   return ( "mozFullScreen" in document &&  document.mozFullScreen )
+      || ( "webkitIsFullScreen" in document && document.webkitIsFullScreen );
+}
+
+// Whack fullscreen
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+
+document.getElementById("fullscreen").addEventListener("click", function(){
+  launchIntoFullscreen(document.documentElement);
+}, false);
