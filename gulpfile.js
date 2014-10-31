4b7006e2a88fc0e968c6607938b08359befa6d5d
Étienne Vallette d'Osia <evo@zengularity.com>
2014-10-31 18:45:51 +0900

    Use ES6

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+/* jshint asi:true */
+
+var http = require('http')
+var path = require('path')
+
+var browserify = require('gulp-browserify')
+var concat = require('gulp-concat')
+var del = require('del')
+var nib = require('nib')
+var ecstatic = require('ecstatic')
+var gulp = require('gulp')
+var stylus = require('gulp-stylus')
+var watch = require('gulp-watch')
+var to5Browserify = require("6to5-browserify")
+
+gulp.task('clean', function(done) {
+  del(['build'], done)
+});
+
+gulp.task('css', ['clean'], function() {
+  return gulp.src(['src/css/**/*.styl'])
+    .pipe(stylus({use: [nib()]}))
+    .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('js', ['clean'], function() {
+  return gulp.src(path.join(__dirname, "src/js/main.js"))
+    .pipe(browserify({transform: ['6to5-browserify']}))
+    .pipe(concat("main.js"))
+    .pipe(gulp.dest(path.join(__dirname, "dist/js")))
+})
+
+gulp.task('build', ['js', 'css'])
+
+gulp.task('watch', ['build'], function () {
+  gulp.watch(path.join(__dirname, "src/js/*"), ['js'])
+  gulp.watch(path.join(__dirname, "src/css/*.styl"), ['css'])
+})
+
+gulp.task('serve', function () {
+  var port = Number(process.env.PORT || 8282)
+  http.createServer(ecstatic({
+    root: process.cwd(),
+    baseDir: '/',
+    cache: 1,
+    showDir: false,
+    autoIndex: true,
+    defaultExt: 'html'
+  })).listen(port)
+  console.info("Listen connection on http://127.0.0.1:" + port)
+})
+
+gulp.task('default', ['watch', 'serve'])
